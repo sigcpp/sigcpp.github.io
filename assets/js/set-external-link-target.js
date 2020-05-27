8e2f4f852e2d65f01c912a8b9553ac6d3ba9a836
Sean Murthy <murthys@wcsu.edu>
2020-05-26 23:34:00 -0400

    Visit only a elements; add external link indicator

--- assets/js/set-external-link-target.js
+++ assets/js/set-external-link-target.js
@@ -2,20 +2,26 @@
 * set-external-link-target.js
 * Sean Murthy
 *
-* Change the target attribute of all external links to "_blank"
+* Change the target attribute of external links to "_blank" and add a title and a small
+* NE arrow to let the user know that links open in a new tab
+* - include only links attached to "a" element
 * - exclude links that already have a target (to allow customization in source)
 *
-* Call function setExternalLinkTarget at document onload 
+* Assumes an appropriate style is set for the span element inserted
 *
-* Core solution from: https://stackoverflow.com/a/4425214
+* Function setExternalLinkTarget is expected to be called on document load 
 */
 
 function setExternalLinkTarget() {
-    var links = document.links;
-
-    for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-        if (links[i].hostname != window.location.hostname && links[i].target.length == 0) {
-            links[i].target = '_blank';
-        } 
+    var anchors = document.getElementsByTagName("A");
+    var host_name = window.location.hostname;
+    
+    for (var i = 0, anchorsLength = anchors.length; i < anchorsLength; i++) {
+        var anchor = anchors[i];
+        if (anchor.hostname != host_name && anchor.target.length == 0) {
+            anchor.target = '_blank';
+            anchor.title = 'Opens in new tab';
+            anchor.insertAdjacentHTML('afterend', '<span class="external">+</span>');
+       }
     }
 }
